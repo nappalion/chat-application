1bbbce133f47fb949cec10451904d137784c7ee6
nipplelion <piolotorrico@yahoo.com>
2022-11-12 17:20:46 -0800

    Add print statements, messages, and main method to run code

--- chat-application.py
+++ chat-application.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import select
 
+RECV_BUFFER = 4096
 port = 80
 socketList = []
 
@@ -21,9 +22,11 @@ def server():
   serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   serverSocket.bind(('', port))
-  socket.listen(1)
+  serverSocket.listen(10)
 
   socketList.append(serverSocket)
+
+  print("Server starting on port " + str(port))
 
   while True:
     read, write, error = select.select(socketList, [], [], 0)
@@ -31,8 +34,12 @@ def server():
     for sock in read:
       if sock == serverSocket:
         # do something
+
         sockOb, address = serverSocket.accept()
         socketList.append(sockOb)
+        print("Client %s connected" % address)
+
+        sendMessage(serverSocket, sockOb, "%s entered the chatting room\n" % address)
 
       else:
 
@@ -41,10 +48,18 @@ def server():
 
           if(data):
             sendMessage(serverSocket, sock, '\r' + str(sock.getpeername() + data))
+
           else:
             if(sock in socketList):
               socketList.remove(sock)
 
+            sendMessage(serverSocket, sock, "Client %s is offline\n" % address)
+
         except:
-          ''
-        return
+          sendMessage(serverSocket, sock, "Client %s is offline\n" % address)
+          continue
+
+  serverSocket.close()
+
+if __name__ == "__main__":
+  sys.exit(server())
